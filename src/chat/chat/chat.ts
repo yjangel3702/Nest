80f5d800509c181361757c54529ddb3a5e7aa9cf
kimyujung <yjangel3702@gmail.com>
2023-10-13 11:49:16 +0900

    아아ㅏ

--- src/chat/chat/chat.ts
+++ src/chat/chat/chat.ts
@@ -1,11 +1,36 @@
-import { Injectable } from '@nestjs/common';
-import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { Injectable, Logger } from '@nestjs/common';
+import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+interface MessageDto {
+  room: string;
+  nickname: string;
+  message: string;
+}
 
 @WebSocketGateway(4020, { transports: ['websocket', 'polling'], cors: { origin: '*' } })
 export class Chat {
 
   @WebSocketServer()
   server: Server;
+  logger = new Logger();
+
+  @SubscribeMessage('join')
+  handlerJoin(
+    @MessageBody() room: string,
+    @ConnectedSocket() socket: Socket
+  ): void {
+    this.logger.warn(`Join Room: ${room}`);
+    socket.join(room);
+  }
+
+  @SubscribeMessage('send')
+  hanlerSend(
+    @MessageBody() messageDto : MessageDto
+  ): void {
+    const { room, message, nickname } = messageDto;
+    this.logger.verbose(message);
+    this.server.to(room).emit('get', {nickname, message});
+  }
 
 }
